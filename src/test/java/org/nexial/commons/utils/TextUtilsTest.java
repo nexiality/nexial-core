317b7572eb9741e0f32fcd9403a7cce628426e7e
automike <mikeliucc@users.noreply.github.com>
2019-05-15 11:44:27 -0700

    fix failed tests
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- src/test/java/org/nexial/commons/utils/TextUtilsTest.java
+++ src/test/java/org/nexial/commons/utils/TextUtilsTest.java
@@ -535,14 +535,14 @@ public class TextUtilsTest {
     @Test
     public void base64() {
         Assert.assertEquals("WVBxVFd6ejlWbkZidGNsNE1hYmpOaDZRRW5nak5OQUg6UlNkRnRSYWdBZmtIZnNPbw==",
-                            TextUtils.base64encoding("YPqTWzz9VnFbtcl4MabjNh6QEngjNNAH:RSdFtRagAfkHfsOo"));
+                            TextUtils.base64encode("YPqTWzz9VnFbtcl4MabjNh6QEngjNNAH:RSdFtRagAfkHfsOo"));
         Assert.assertEquals("UVJHZ2tCRDNZSHp2SGdBMGVhSWgyWEd4R2VBM0FHb1k6VVV1QUhOY0xlb00yZTBWZg==",
-                            TextUtils.base64encoding("QRGgkBD3YHzvHgA0eaIh2XGxGeA3AGoY:UUuAHNcLeoM2e0Vf"));
+                            TextUtils.base64encode("QRGgkBD3YHzvHgA0eaIh2XGxGeA3AGoY:UUuAHNcLeoM2e0Vf"));
 
         Assert.assertEquals("YPqTWzz9VnFbtcl4MabjNh6QEngjNNAH:RSdFtRagAfkHfsOo",
-                            TextUtils.base64decoding("WVBxVFd6ejlWbkZidGNsNE1hYmpOaDZRRW5nak5OQUg6UlNkRnRSYWdBZmtIZnNPbw=="));
+                            TextUtils.base64decode("WVBxVFd6ejlWbkZidGNsNE1hYmpOaDZRRW5nak5OQUg6UlNkRnRSYWdBZmtIZnNPbw=="));
         Assert.assertEquals("QRGgkBD3YHzvHgA0eaIh2XGxGeA3AGoY:UUuAHNcLeoM2e0Vf",
-                            TextUtils.base64decoding("UVJHZ2tCRDNZSHp2SGdBMGVhSWgyWEd4R2VBM0FHb1k6VVV1QUhOY0xlb00yZTBWZg=="));
+                            TextUtils.base64decode("UVJHZ2tCRDNZSHp2SGdBMGVhSWgyWEd4R2VBM0FHb1k6VVV1QUhOY0xlb00yZTBWZg=="));
     }
 
     @Test
