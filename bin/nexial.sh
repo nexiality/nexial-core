676836cd0545fd8a3d686d31782bd8b3693af4e0
automike <mikeliucc@users.noreply.github.com>
2020-09-08 16:27:26 -0700

    minor batch file update to correctly capture exit code after running nexial
    Signed-off-by: automike <mikeliucc@users.noreply.github.com>

--- bin/nexial.sh
+++ bin/nexial.sh
@@ -6,46 +6,44 @@
 # NEXIAL_OUTPUT       - the output directory (optional)
 # --------------------------------------------------------------------------------
 
-NEXIAL_HOME=$(cd `dirname $0`/..; pwd -P)
+NEXIAL_HOME=$(
+  cd $(dirname $0)/..
+  pwd -P
+)
 . ${NEXIAL_HOME}/bin/.commons.sh
 title "nexial runner"
 checkJava
 resolveEnv
 
-
 # setting project classpath (classes & lib)
 # giving priority to project-specific classpaths
 PROJECT_CLASSPATH=
 if [[ -d "${PROJECT_HOME}" ]]; then
-	PROJECT_CLASSPATH="${PROJECT_HOME}/classes:${PROJECT_HOME}/lib/*"
+  PROJECT_CLASSPATH="${PROJECT_HOME}/classes:${PROJECT_HOME}/lib/*"
 else
-	PROJECT_CLASSPATH=
+  PROJECT_CLASSPATH=
 fi
 
-if [[ "${CHROME_BIN}" = "" ]]; then CHROME_BIN="${DEFAULT_CHROME_BIN}"; fi
+if [[ "${CHROME_BIN}" == "" ]]; then CHROME_BIN="${DEFAULT_CHROME_BIN}"; fi
 echo "setting CHROME_BIN  as ${CHROME_BIN}"
 
-if [[ "${FIREFOX_BIN}" = "" ]]; then FIREFOX_BIN="${DEFAULT_FIREFOX_BIN}"; fi
+if [[ "${FIREFOX_BIN}" == "" ]]; then FIREFOX_BIN="${DEFAULT_FIREFOX_BIN}"; fi
 echo "setting FIREFOX_BIN as ${FIREFOX_BIN}"
-
 
 # support JVM max mem config
 if [[ -n "${NEXIAL_MAX_MEM}" ]]; then export MAX_MEM=-Xmx${NEXIAL_MAX_MEM}; fi
 
-
 # support environment default for output base directory
-if [[ ! -z "${NEXIAL_OUTPUT}" ]] ; then
-    mkdir -p "${NEXIAL_OUTPUT}"
-    export JAVA_OPT="${JAVA_OPT} -Dnexial.defaultOutBase=${NEXIAL_OUTPUT}"
+if [[ ! -z "${NEXIAL_OUTPUT}" ]]; then
+  mkdir -p "${NEXIAL_OUTPUT}"
+  export JAVA_OPT="${JAVA_OPT} -Dnexial.defaultOutBase=${NEXIAL_OUTPUT}"
 fi
-
 
 # sync nexial execution stats to environment variables
-if [[ "${NEXIAL_POST_EXEC_SHELL}" != "" ]] ; then
-    echo "setting post exec shell script to ${NEXIAL_POST_EXEC_SHELL}"
-    export JAVA_OPT="${JAVA_OPT} -Dnexial.postExecEnv=${NEXIAL_POST_EXEC_SHELL}"
+if [[ "${NEXIAL_POST_EXEC_SHELL}" != "" ]]; then
+  echo "setting post exec shell script to ${NEXIAL_POST_EXEC_SHELL}"
+  export JAVA_OPT="${JAVA_OPT} -Dnexial.postExecEnv=${NEXIAL_POST_EXEC_SHELL}"
 fi
-
 
 # run nexial now
 echo
@@ -71,19 +69,19 @@ echo
 
 UPDATE_AGREED=${USER_NEXIAL_INSTALL}/update-now
 if test -f "$UPDATE_AGREED"; then
-    presentDir=eval "pwd"
-    echo "Updating nexial-core..."
-    rm -rf "$UPDATE_AGREED"
-    eval "${JAVA} -jar $NEXIAL_INSTALLER_HOME/lib/nexial-installer.jar upgradeNexial"
-    cd "$presentDir"
+  presentDir=eval "pwd"
+  echo "Updating nexial-core..."
+  rm -rf "$UPDATE_AGREED"
+  eval "${JAVA} -jar $NEXIAL_INSTALLER_HOME/lib/nexial-installer.jar upgradeNexial"
+  cd "$presentDir"
 fi
 
 RESUME_FILE=${USER_NEXIAL_INSTALL}/resume-after-update
 if test -f "$RESUME_FILE"; then
-    echo "Resuming nexial-core execution."
-    rm -rf "$RESUME_FILE"
-    eval "$NEXIAL_HOME/bin/nexial.sh $*"
-    rc=$?
+  echo "Resuming nexial-core execution."
+  rm -rf "$RESUME_FILE"
+  eval "$NEXIAL_HOME/bin/nexial.sh $*"
+  rc=$?
 fi
 
 echo
