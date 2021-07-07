fb568baa7a7f61c469911f0f414d1038bf44ed1d
automike <mikeliucc@gmail.com>
2021-07-07 01:10:54 -0700

    reinstate failed test... need RCA
    Signed-off-by: automike <mikeliucc@gmail.com>

--- src/test/java/org/nexial/core/model/NexialFilterTest.java
+++ src/test/java/org/nexial/core/model/NexialFilterTest.java
@@ -265,11 +265,11 @@ public class NexialFilterTest {
             Assert.assertTrue(NexialFilter.newInstance("file1 is readable-file").isMatch(tmpFilePath));
             Assert.assertTrue(NexialFilter.newInstance("file1 is not readable-file").isMatch(tmpFilePath + "1"));
             Assert.assertTrue(NexialFilter.newInstance("file1 has file-size 0").isMatch(tmpFilePath));
-            // Assert.assertTrue(NexialFilter.newInstance("dir1 is readable-path").isMatch(JAVA_IO_TMPDIR));
-            // Assert.assertTrue(NexialFilter.newInstance("dir1 is not readable-path").isMatch(JAVA_IO_TMPDIR + "2"));
-            // Assert.assertTrue(NexialFilter.newInstance("dir1 is not empty-path").isMatch(JAVA_IO_TMPDIR));
-            // Assert.assertTrue(NexialFilter.newInstance("dir1 is not empty-path").isMatch(JAVA_IO_TMPDIR + "2"));
-            // Assert.assertTrue(NexialFilter.newInstance("dir1 is empty-path").isMatch(tmpDir));
+            Assert.assertTrue(NexialFilter.newInstance("dir1 is readable-path").isMatch(JAVA_IO_TMPDIR));
+            Assert.assertTrue(NexialFilter.newInstance("dir1 is not readable-path").isMatch(JAVA_IO_TMPDIR + "2"));
+            Assert.assertTrue(NexialFilter.newInstance("dir1 is not empty-path").isMatch(JAVA_IO_TMPDIR));
+            Assert.assertTrue(NexialFilter.newInstance("dir1 is not empty-path").isMatch(JAVA_IO_TMPDIR + "2"));
+            Assert.assertTrue(NexialFilter.newInstance("dir1 is empty-path").isMatch(tmpDir));
         } finally {
             FileUtils.deleteQuietly(tmpFile);
             FileUtils.deleteDirectory(tmpPath);
