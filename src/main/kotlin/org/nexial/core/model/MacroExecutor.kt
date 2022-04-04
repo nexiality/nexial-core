376db76041670341b78634907baef447d7c7bec1
dhana555std <dhanapathi.marepalli@gmail.com>
2022-04-04 07:22:28 +0530

    Adding changes for skipped tests Macro.

--- src/main/kotlin/org/nexial/core/model/MacroExecutor.kt
+++ src/main/kotlin/org/nexial/core/model/MacroExecutor.kt
@@ -112,6 +112,8 @@ class MacroExecutor(private val initialTestStep: TestStep, val macro: Macro,
             if (!result.isSkipped)
                 context.setData(OPT_LAST_OUTCOME, succeed)
             else {
+                activitySummary.adjustSkipCount(1)
+                activitySummary.adjustTotalSteps(1)
                 // if macro step is part of repeat until and is skipped don't reduce total steps
                 if (!initialTestStep.macroPartOfRepeatUntil) activitySummary.adjustTotalSteps(-1)
                 if (testStep.commandFQN == CMD_SECTION) {
