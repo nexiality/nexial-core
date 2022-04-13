3d990f90d77d251754b1f647ee4e396c6e9ba2b7
dhana555std <dhanapathi.marepalli@gmail.com>
2022-04-13 11:18:22 +0530

    Execution Summary Documentation changes and code refactoring.

--- src/main/java/org/nexial/core/model/CommandRepeater.java
+++ src/main/java/org/nexial/core/model/CommandRepeater.java
@@ -178,11 +178,10 @@ public class CommandRepeater {
                 } finally {
                     boolean isSkipped = result != null && result.isSkipped();
 
-                    // time tracking
-                    if (!isSkipped) { trackTimeLogs.checkEndTracking(context, testStep); }
-
                     // expand substitution in description column
                     if (!isSkipped) {
+                        // time tracking
+                        trackTimeLogs.checkEndTracking(context, testStep);
                         List<XSSFCell> row = testStep.getRow();
                         XSSFCell cellDescription = row.get(COL_IDX_DESCRIPTION);
                         String description = Excel.getCellValue(cellDescription);
